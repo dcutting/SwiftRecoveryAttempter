016eb90efb2b136ffb349d5fc077c67f9946b060
Dan Cutting <dan@cutting.io>
2016-08-22 09:33:56 +0100

    Initial Commit

---
+++ SwiftRecoveryAttempter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftRecoveryAttempter
+//
+//  Created by Dan Cutting on 22/08/2016.
+//  Copyright © 2016 cutting.io. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
