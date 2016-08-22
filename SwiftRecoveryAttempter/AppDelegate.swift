2cf391a86aca7da4da138a43173429ca90d02014
Dan Cutting <dan@cutting.io>
2016-08-22 10:38:21 +0100

    Fix errors remaining after Swift 3 conversion.

--- SwiftRecoveryAttempter/AppDelegate.swift
+++ SwiftRecoveryAttempter/AppDelegate.swift
@@ -42,11 +42,11 @@ let errorModal = ErrorModal.app
 }
 
 class RecoveryAttempter: NSObject {
-  override func attemptRecovery(fromError error: NSError, optionIndex recoveryOptionIndex: Int, delegate: AnyObject?, didRecoverSelector: Selector?, contextInfo: UnsafeMutablePointer<Void>?) {
+  override func attemptRecovery(fromError error: Error, optionIndex recoveryOptionIndex: Int, delegate: AnyObject?, didRecoverSelector: Selector?, contextInfo: UnsafeMutablePointer<Void>?) {
     print("Attempting recovery for window modal error.")
   }
 
-  override func attemptRecovery(fromError error: NSError, optionIndex recoveryOptionIndex: Int) -> Bool {
+  override func attemptRecovery(fromError error: Error, optionIndex recoveryOptionIndex: Int) -> Bool {
     print("Attempting recovery for app modal error.")
     return true
   }
